73c0df42469ca38ea4641ffaf1b028b1ea6791f1
Ayush3101 <singhayush0131@gmail.com>
2024-05-21 11:56:05 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Footer = () => {
+  return <footer>
+    <div>
+        <h1>TECHSTAR</h1>
+        <p>@All Right Reserved</p>
+      
+    </div>
+    <div>
+        <h5>Follow me</h5>
+        <div>
+            <a href="https://Instagram.com/its_rajputboy" target={'blank'}>
+                Instagram
+                </a>
+            <a href="https://www.linkedin.com/in/ayush-ayush-2a43651b9/"target={'blank'}>
+                Linkedin
+                </a>
+            <a href="https://Github.com/Ayush3101"target={'blank'}>
+                Github
+                </a>
+        </div>
+    </div>
+    </footer>
+  
+}
+
+export default Footer
---
+++ src/components/Home.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import "../styles/home.scss";
+import vg from "../assets/2.webp";
+import {AiFillGoogleCircle,AiFillAmazonCircle, AiFillYoutube,AiFillInstagram}from "react-icons/ai"
+
+const Home = () => {
+  return (
+    <>
+    <div className='home' id='home'>
+        <main>
+            <h1>TechyStar</h1>
+            <p>Solutions to all your  problems</p>
+        </main>
+      
+    </div>
+
+    <div className="home2">
+        <img src={vg} alt="Graphics"/>
+
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis illum recusandae sed quae cupiditate ad, omnis numquam illo et quos aperiam atque? Repellat placeat at delectus expedita? Unde minima ducimus minus, quos laboriosam perspiciatis ratione laborum nisi nesciunt recusandae, explicabo earum voluptates repellendus atque porro accusantium provident eligendi optio dolor.
+            </p>
+        </div>
+    </div>
+    <div className="home3" id="about">
+        <div>
+            <h1>Who we are?</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi deleniti ipsum quasi rem magni repellendus odit veritatis, maxime ipsam, laudantium soluta aperiam, quo dicta. Quos accusantium ex dolorem? Veritatis, modi?</p>
+        </div>
+    </div>
+    <div className="home4" id="brands">
+     <div>
+        <h1>Brands</h1>
+        <article>
+            <div style={{
+                animationDelay:"0.3s",
+            }}
+            >
+                <AiFillGoogleCircle/>
+
+                <p>
+                Google
+                    </p>
+            </div>
+    
+            <div style={{
+                animationDelay:"0.5s",
+            }}
+            >
+                <AiFillAmazonCircle/>
+                <p>Amazon</p>
+            </div>
+            <div style={{
+                animationDelay:"0.7s",
+            }}
+            >
+                <AiFillYoutube/>
+                <p>Youtube</p>
+            </div>
+            <div style={{
+                animationDelay:"1s",
+            }}
+            >
+                <AiFillInstagram/>
+                <p>Instagram</p>
+            </div>
+        </article>
+     </div>   
+    </div>
+    </>
+  )
+}
+
+export default Home
